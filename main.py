d9654ca8eb62c0e0a7f1088579d9a650ac6efa68
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-02-13 09:52:54 -0800

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import sqlite3
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+CSV_PATH = ""
+DB_PATH = ""
 
+conn = sqlite3.connect(DB_PATH)
+cursor = conn.cursor()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
