3ecef6cffde2b610a6b3d73daa52ee88cd1406a3
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-02-13 12:38:48 -0800

    modified methods to be more generic and scalable. 1 method remain, df_to_sql()

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ import pandas as pd
 CSV_FILENAME = "input_data/Sales.csv"
 DB_NAME = "database_files/sample.db"
 DB_TABLE = "sample"
-
+TABLE_COLUMNS = []
 conn = sqlite3.connect(DB_NAME)
 cursor = conn.cursor()
+reader = pd.read_csv(CSV_FILENAME)
+df = pd.DataFrame(reader)
 
 
-def read_csv_and_make_df(filename):
-    reader = pd.read_csv(filename)
-    df = pd.DataFrame(reader)
-    return df
-
-
-def send_to_sql_db():
-    df = read_csv_and_make_df(filename=CSV_FILENAME)
+def df_to_sql():
     sql_db = df.to_sql(name=DB_TABLE, con=conn, if_exists="replace", index=False)
+    for item in df.columns:
+        TABLE_COLUMNS.append(item)
     return sql_db
 
 
-send_to_sql_db()
+
+df_to_sql()
+print(TABLE_COLUMNS)
