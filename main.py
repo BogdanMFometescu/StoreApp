77e0444c067526af813e7e22c47b3c3ad6134250
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-02-13 10:27:54 -0800

    added 2 methods : 1 for reading csv, 1 for sending data to sqlite database.

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import sqlite3
 import pandas as pd
 
-CSV_PATH = ""
-DB_PATH = ""
+CSV_FILENAME = "input_data/Sales.csv"
+DB_NAME = "database_files/sample.db"
+DB_TABLE = "sample"
 
-conn = sqlite3.connect(DB_PATH)
+conn = sqlite3.connect(DB_NAME)
 cursor = conn.cursor()
 
+
+def read_csv_and_make_df(filename):
+    reader = pd.read_csv(filename)
+    df = pd.DataFrame(reader)
+    return df
+
+
+def send_to_sql_db():
+    df = read_csv_and_make_df(filename=CSV_FILENAME)
+    sql_db = df.to_sql(name=DB_TABLE, con=conn, if_exists="replace", index=False)
+    return sql_db
+
+
+send_to_sql_db()
