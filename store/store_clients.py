8e7a67ed500b4ae44129adadad0fb833a2ea833c
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-02-26 02:52:41 -0800

    Added Store clients module

--- store/store_clients.py
+++ store/store_clients.py
@@ -1,5 +1,25 @@
-
-
 class StoreClients:
-    def __init__(self):
-        pass
+    def __init__(self, first_name: str, last_name: str, account_balance: float):
+        assert first_name.isspace() is False and first_name != "", f"{ValueError}Client first_name must be a String "
+        assert last_name.isspace() is False and last_name != "", f"{ValueError}Client last_name must be a String "
+        assert account_balance >= 0, f"{ValueError} Account balance must be greater or equal to zero"
+
+        self._first_name = first_name
+        self._last_name = last_name
+        self._account_balance = account_balance
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self._first_name = value
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self._last_name = value
